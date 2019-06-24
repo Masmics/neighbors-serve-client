90395b6b28abe5fd6da4b1ad9fc095b2fb5f3a18
Masmics <cherlocker3113@gmail.com>
2019-06-23 20:14:13 -0700

    refactored tasksApi env stuff into separate module

--- src/services/tasksApi.js
+++ src/services/tasksApi.js
@@ -1,13 +1,4 @@
-export const createTask = task => {
-  return fetch('http://localhost:7891/api/v1/tasks', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(task)
-  })
-    .then(res => ([res.ok, res.json()]))
-    .then(([ok, json]) => {
-      if(!ok) throw 'unable to post a task';
-      return json;
-    });
-};
+import { post } from './request';
+
+export const createTask = task => post('/api/v1/tasks', task);
 
