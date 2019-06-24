9661d8cad443a8cfbba0d8e12ff0853ceef5d27b
Masmics <cherlocker3113@gmail.com>
2019-06-23 18:13:09 -0700

    client and server connect locally, tests passing

--- src/services/tasksApi.js
+++ src/services/tasksApi.js
@@ -1,9 +1,12 @@
 export const createTask = task => {
-  return Promise.resolve({
-    _id: '0000',
-    ...task
-  });
+  return fetch('http://localhost:7891/api/v1/tasks', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(task)
+  })
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'unable to post a task';
+      return json;
+    });
 };
-
-// dummy data
-
