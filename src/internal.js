52f69946f5a44327674988323c873a4d1b66a8d5
Masmics <cherlocker3113@gmail.com>
2019-07-02 16:53:44 -0700

    deleted README.md as an experiment

---
+++ src/internal.js
@@ -0,0 +1,11 @@
+// import { getTasks, createTask } from './actions/taskActions';
+// import { getTask } from './actions/taskDetailActions';
+
+// import { tasksApi } from './services/tasksApi';
+
+// import store from './store';
+// import reducer from './reducers/index';
+// import tasks from './reducers/taskReducer';
+// import task from './reducers/taskDetailReducer';
+
+// export const { getTasks, createTask};
